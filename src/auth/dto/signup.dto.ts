a9a2c49e527a7ee4db4c60e484be6d269b4edbb4
Chamika Edirisinghe <edirisinghe.chamika.madushan@gmail.com>
2023-10-31 14:25:40 +0530

    Added validation with class validator

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignupDto {
+  @IsNotEmpty()
   firstName: string;
+
   lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
 }
